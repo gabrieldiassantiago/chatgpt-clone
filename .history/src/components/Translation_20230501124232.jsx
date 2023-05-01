cde87be4b94ba61358a96ffd980eace4084c8a5d
Gabriel Dias <gabriel_dias_santiago@hotmail.com>
2023-05-01 12:46:30 -0300

    first commit

---
+++ .history/src/components/Translation_20230501124232.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Translation({ doStuff, setInput, result }) {
+  return (
+    <div>
+      <h1>Coloque sua pergunte</h1>
+      <textarea
+        className="text-area"
+        cols={55}
+        rows={10}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <button className="action-btn" onClick={doStuff}>
+       Enviar
+      </button>
+
+      <h3 className="result-text">{result.length > 0 ? result : ""}</h3>
+    </div>
+  );
+}
